c95878385a1b11305fc55eb8133f5e249c275d2a
Lee-Junhee <jlee02@stuy.edu>
2020-02-10 11:00:01 -0500

    removed unnecessary code

--- engine/__main__.py
+++ engine/__main__.py
@@ -4,14 +4,4 @@ from line import Line
 
 p = Picture('pic.ppm', 128, 128, 255)
 
-c = Color(
-    lambda x, y: 255,
-    lambda x, y: 255,
-    lambda x, y: 255
-    )
-
-p.addcolor(c)
-
-Line.draw(p, c, 0, 100, 100, 13)
-
 p.commit()
